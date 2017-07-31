c4816bdb5e3d756c67762b463ad357aa46ce53dd
crossoverJie <crossoverjie@gmail.com>
2017-07-31 19:21:00 +0800

    :sparkles:  init

---
+++ sbc-request-check/src/main/java/com/crossoverJie/request/check/requestcheck/RequestCheckApplication.java
@@ -0,0 +1,12 @@
+package com.crossoverJie.request.check.requestcheck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RequestCheckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RequestCheckApplication.class, args);
+	}
+}
---
+++ sbc-request-check/src/test/java/com/crossoverJie/request/check/requestcheck/RequestCheckApplicationTests.java
@@ -0,0 +1,16 @@
+package com.crossoverJie.request.check.requestcheck;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RequestCheckApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
