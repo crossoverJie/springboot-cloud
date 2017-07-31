5e5fb3e21b6f98b680e7a5ce57d66881c5527205
crossoverJie <crossoverjie@gmail.com>
2017-08-01 00:43:02 +0800

    :ambulance: add redis status check

---
+++ sbc-request-check/src/main/java/com/crossoverJie/request/check/conf/CheckReqCondition.java
@@ -0,0 +1,35 @@
+package com.crossoverJie.request.check.conf;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Condition;
+import org.springframework.context.annotation.ConditionContext;
+import org.springframework.core.type.AnnotatedTypeMetadata;
+import org.springframework.util.StringUtils;
+
+/**
+ * Function:校验是否有redis配置
+ *
+ * @author crossoverJie
+ *         Date: 2017/8/1 00:22
+ * @since JDK 1.8
+ */
+public class CheckReqCondition implements Condition {
+
+    private static Logger logger = LoggerFactory.getLogger(CheckReqCondition.class);
+
+
+    @Override
+    public boolean matches(ConditionContext context, AnnotatedTypeMetadata annotatedTypeMetadata) {
+
+        //如果没有加入redis配置的就返回false
+        String property = context.getEnvironment().getProperty("spring.redis.host");
+        if (StringUtils.isEmpty(property)){
+            logger.warn("Need to configure redis!");
+            return false ;
+        }else {
+            return true;
+        }
+
+    }
+}
--- sbc-request-check/src/main/java/com/crossoverJie/request/check/conf/CheckReqConf.java
+++ sbc-request-check/src/main/java/com/crossoverJie/request/check/conf/CheckReqConf.java
@@ -1,9 +1,13 @@
 package com.crossoverJie.request.check.conf;
 
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan("com.crossoverJie.request.check.interceptor")
+
+//是否有redis配置的校验，如果没有配置则不会加载改配置，也就是当前插件并不会生效
+@Conditional(CheckReqCondition.class)
 public class CheckReqConf {
 }
