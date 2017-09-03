88c1253759b7c6302e6f40b90ef241c1c9f16afd
crossoverJie <crossoverjie@gmail.com>
2017-09-04 01:44:29 +0800

    :sparkles: /hystrix.stream

--- sbc-user/user/src/main/java/com/crossoverJie/sbcuser/SbcUserApplication.java
+++ sbc-user/user/src/main/java/com/crossoverJie/sbcuser/SbcUserApplication.java
@@ -6,9 +6,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 @EnableDiscoveryClient
 @SpringBootApplication
+
+//开启 /hystrix.stream访问
+@EnableHystrix
 
 //扫描feign包
 @EnableFeignClients(basePackages = {"com.crossoverJie.order.feign.api"})
