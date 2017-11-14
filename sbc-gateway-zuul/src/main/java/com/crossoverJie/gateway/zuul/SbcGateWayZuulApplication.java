7ad921df7595303e36cc2b9a1cf8cf6df2b9a6eb
crossoverJie <crossoverjie@gmail.com>
2017-11-15 01:38:25 +0800

    :memo: moment

--- sbc-gateway-zuul/src/main/java/com/crossoverJie/gateway/zuul/SbcGateWayZuulApplication.java
+++ sbc-gateway-zuul/src/main/java/com/crossoverJie/gateway/zuul/SbcGateWayZuulApplication.java
@@ -2,14 +2,17 @@ package com.crossoverJie.gateway.zuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
-import org.springframework.cloud.netflix.turbine.EnableTurbine;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
-
-
-@EnableTurbine
+/**
+ *
+ * @author crossoverJie
+ * @date 2017/11/14
+ */
 @SpringBootApplication
-@EnableHystrixDashboard
+
+//开启zuul代理
+@EnableZuulProxy
 public class SbcGateWayZuulApplication {
 
 	public static void main(String[] args) {
