8f212d568abd819d1a7df9193cd4a668dd86327e
crossoverJie <crossoverjie@gmail.com>
2017-11-15 01:41:15 +0800

    :wrench: config two

--- sbc-gateway-zuul/src/main/java/com/crossoverJie/gateway/zuul/SbcGateWayZuulApplication.java
+++ sbc-gateway-zuul/src/main/java/com/crossoverJie/gateway/zuul/SbcGateWayZuulApplication.java
@@ -1,4 +1,4 @@
-package com.crossoverJie.gateway.zuul;
+package com.crossoverjie.gateway.zuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
