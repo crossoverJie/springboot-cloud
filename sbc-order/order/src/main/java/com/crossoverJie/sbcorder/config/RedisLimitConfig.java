75f896db1ebabb3e3e288751ffe584041d82b024
crossoverJie <crossoverjie@gmail.com>
2018-04-24 22:40:37 +0800

    :sparkles: Introducing new features.兼容 redis 集群

--- sbc-order/order/src/main/java/com/crossoverJie/sbcorder/config/RedisLimitConfig.java
+++ sbc-order/order/src/main/java/com/crossoverJie/sbcorder/config/RedisLimitConfig.java
@@ -5,7 +5,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisClusterConnection;
+import org.springframework.data.redis.connection.jedis.JedisClusterConnection;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 import redis.clients.jedis.HostAndPort;
+import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisCluster;
 
 /**
@@ -22,11 +27,14 @@ public class RedisLimitConfig {
     @Value("${redis.limit}")
     private int limit ;
 
+
     @Autowired
-    private JedisCluster jedisCluster ;
+    private JedisConnectionFactory jedisConnectionFactory;
 
     @Bean
     public RedisLimit build(){
+        RedisClusterConnection clusterConnection = jedisConnectionFactory.getClusterConnection();
+        JedisCluster jedisCluster = (JedisCluster) clusterConnection.getNativeConnection();
         RedisLimit redisLimit = new RedisLimit.Builder<>(jedisCluster)
                 .limit(limit)
                 .build();
--- sbc-request-check/src/main/java/com/crossoverJie/request/check/conf/CheckReqConf.java
+++ sbc-request-check/src/main/java/com/crossoverJie/request/check/conf/CheckReqConf.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @ComponentScan("com.crossoverJie.request.check.interceptor,com.crossoverJie.request.check.properties")
 
-//是否有redis配置的校验，如果没有配置则不会加载改配置，也就是当前插件并不会生效
+//是否有 redis 配置的校验，如果没有配置则不会加载改配置，也就是当前插件并不会生效
 @Conditional(CheckReqCondition.class)
 public class CheckReqConf {
 }
