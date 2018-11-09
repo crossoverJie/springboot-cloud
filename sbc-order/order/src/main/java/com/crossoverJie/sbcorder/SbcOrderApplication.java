de105ad05910fe6f8ffacabff2f1f60c08b34cb8
crossoverJie <crossoverjie@gmail.com>
2018-11-10 02:04:13 +0800

    :loud_sound: Adding logs.

--- sbc-order/order/src/main/java/com/crossoverJie/sbcorder/SbcOrderApplication.java
+++ sbc-order/order/src/main/java/com/crossoverJie/sbcorder/SbcOrderApplication.java
@@ -3,12 +3,10 @@ package com.crossoverJie.sbcorder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 
 @EnableCircuitBreaker
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 @SpringBootApplication
 @ComponentScan(value = "com.crossoverJie.sbcorder,com.crossoverjie.distributed.intercept")
 public class SbcOrderApplication {
